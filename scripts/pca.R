4cdad9240a837e84ab8700f34289e2e535ead72f
jcoliver <jcoliver@email.arizona.edu>
2016-09-26 16:49:44 -0700

    Add comments & rearrange data preparation

--- scripts/pca.R
+++ scripts/pca.R
@@ -3,15 +3,20 @@
 # jcoliver@email.arizona.edu
 # 2016-09-26
 
+# TODO: Add reference to tutorial pdfs for adegenet
+
 ################################################################################
 # SUMMARY
 # * Reads in genetic data from genind object; also reads in locality information
 # for plot coloring purposes
 # * Performs Principal Components Analysis (PCA) on allele frequency data
 # * Plots first three axes of PCA (1 & 2 in one plot, 2 & 3 in a second plot)
+# * Runs preliminary Principal Coordinates Analyses (PCoA), which appears to 
+# produce results identical to PCA.
 
 ################################################################################
 # SETUP
+# Load dependancies, load data, and extract allele frequencies
 
 # Load dependancies
 #install.packages("adegenet")
@@ -25,22 +30,14 @@ plot.out.file = "output/pca-plots.pdf"
 # Read in genind file
 load(file = genind.file)
 
+################################################################################
+# DATA PREP
+# Extract allele frequencies & set up color vector for coloring plots
+
 # Extract allele frequency data, replacing NA values with mean allele 
 # frequencies (mean across individuals of all populations)
 allele.data <- tab(x = apo.str.genind, freq = TRUE, NA.method = "mean")
 
-################################################################################
-# PCA
-
-# Run pca, skipping the scaling step (scale = FALSE), because we already scaled
-# allele frequencies via tab call above.
-allele.pca <- dudi.pca(df = allele.data, 
-                       center = TRUE, 
-                       scale = FALSE, 
-                       scannf = FALSE, 
-                       nf = 3)
-
-# Create plots
 # Load localities (for coloring north & south populations differently)
 load(file = localities.file)
 pop.nums <- pop(apo.str.genind)
@@ -60,13 +57,26 @@ north.color.endpoints <- c("#DF0000", "#7C3737")
 north.colors <- colorRampPalette(colors = north.color.endpoints)(length(north.pop.nums))
 col.vector <- c(south.colors, north.colors)
 
-################################################################################
 # Use population number as element names in col.vector so we can re-order the 
 # elements in col.vector based on levels in the pop.vector; necessary so the 
 # populations are actually colored as we expect them to be
 names(col.vector) <- c(south.pop.nums, north.pop.nums)
 col.vector <- col.vector[levels(pop.nums)]
 
+################################################################################
+# PCA
+# Run PCA, identify populations for plot coloring purposes, and plot first 
+# three PCA axes (in two graphs)
+
+# Run pca, skipping the scaling step (scale = FALSE), because we already scaled
+# allele frequencies via tab call above.
+allele.pca <- dudi.pca(df = allele.data, 
+                       center = TRUE, 
+                       scale = FALSE, 
+                       scannf = FALSE, 
+                       nf = 3)
+
+# Create plots
 # Open pdf device and send plots
 pdf(file = plot.out.file, useDingbats = FALSE)
 par(mfrow = c(2,1))
