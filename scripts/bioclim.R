392f8214d26d7a8cf9e7967ad4c5f3a059393acc
jcoliver <jcoliver@email.arizona.edu>
2016-11-02 13:15:30 -0700

    First draft of PCA on bioclimate variables

--- scripts/bioclim.R
+++ scripts/bioclim.R
@@ -5,23 +5,47 @@
 
 rm(list = ls())
 ################################################################################
+# Much of this comes from http://rspatial.org/sdm/rst/4_sdm_envdata.html
+
+localities.file = "output/reconciled-localities.RData"
+
 # install.packages("raster")
 library("raster")
 
 # Download climate variables into RasterStack
-bioclim <- getData("worldclim", download = TRUE, path = "bioclim", var = "bio", res = 2.5)
+# For high resolution (res = 0.5), only parts of the data are downloaded; 
+# unfortunately, the cutoff between data sets is longitude -120, so two 
+# downloads are necessary. While this is a east/west division, it effectively 
+# splits along south/north division in our populations (south = east, north = 
+# west).
+bioclim.west <- getData("worldclim", download = TRUE, path = "bioclim", var = "bio", res = 0.5, lat = 40, lon = -121)
+bioclim.east <- getData("worldclim", download = TRUE, path = "bioclim", var = "bio", res = 0.5, lat = 40, lon = -119)
 
 # Pull out values for our localities
+load(file = localities.file)
+coords <- localities[, c("longitude", "latitude")]
+bioclim.east.extract <- extract(x = bioclim.east, y = coords)
+bioclim.west.extract <- extract(x = bioclim.west, y = coords)
 
+# Join into one data frame
+bioclim.vars <- bioclim.east.extract
+bioclim.vars[is.na(bioclim.vars)] <- bioclim.west.extract[is.na(bioclim.vars)]
+rownames(bioclim.vars) <- localities$pop.name
 
-# Check for colinearity among climate variables
+# Check for colinearity among climate variables?
+# Skip because PCA effectively does this?
+# Could still think about this, as it would reduce the number of variables
+# TODO: calculate r^2 among all bioclim var pairs; drop secondaries that have 
+# r^2 > 0.7...
+# raster::pairs(x = bioclim.subset[, 1:4])
+fit <- prcomp(x = bioclim.vars, scale = TRUE)
+summary(fit)
+x.lab <- paste0("PC 1 (", round(fit$sdev[1]/sum(fit$sdev) * 100, 0), "%)")
+y.lab <- paste0("PC 2 (", round(fit$sdev[2]/sum(fit$sdev) * 100, 0), "%)")
 
-# From raster guide help on getData
-# If name='worldclim' you must also provide arguments var, and a resolution res. 
-# Valid variables names are ’tmin’, ’tmax’, ’prec’ and ’bio’.  Valid resolutions 
-# are 0.5, 2.5, 5, and 10 (minutes of a degree).  In the case of res=0.5 , you 
-# must also provide a lon and lat argument for a tile; for the lower resolutions 
-# global data will be downloaded. In all cases there are 12 (monthly) files for 
-# each variable except for ’bio’ which contains 19 files.
-# getData('worldclim', var='tmin', res=0.5, lon=5, lat=45)
-# getData('worldclim', var='bio', res=10)
+# Points
+plot(x = fit$x[, 1], y = fit$x[, 2], xlab = x.lab, ylab = y.lab)
+
+# Pop. names
+plot(x = fit$x[, 1], y = fit$x[, 2], xlab = x.lab, ylab = y.lab, type = "n")
+text(x = fit$x[, 1], y = fit$x[, 2], labels = localities$pop.name)
