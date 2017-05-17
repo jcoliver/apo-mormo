2627a91da24cee0b3ce17eff8076ee95449b08ca
jcoliver <jcoliver@email.arizona.edu>
2017-05-17 10:16:19 -0700

    Separate rarefaction code and tests

--- functions/rarefaction.R
+++ functions/rarefaction.R
@@ -5,47 +5,17 @@
 
 ################################################################################
 # SUMMARY
-# Use Kalinowski's algorithm for correcting for uneven sampling to estimate
-# private allele frequency
+# Kalinowski's algorithms for correcting for uneven sampling to estimate allele 
+# richness and private allele frequency
 
-# calcQ <- function(N, i, j, g) {
-#   Nj <- sum(N[ ,j])
-#   Nij <- N[i, j]
-#   prod <- 1
-#   iter.g <- 0
-#   while (iter.g < g && prod > 0) {
-#     numerator <- Nj - Nij - iter.g
-#     if (numerator == 0) {
-#       prod <- 0
-#     } else {
-#       denominator <- Nj - iter.g
-#       prod <- prod * (numerator / denominator)
-#       iter.g <- iter.g + 1
-#     }
-#   }
-#   return(prod)
-# }
-
-# calcP <- function(N, i, j, g) {
-#   prob <- 1 - calcQ(N = N, i = i, j = j, g = g)
-#   return(prob)
-# }
-
-# # P112 = 1 - Q112
-# calcP(N = N.matrix, i = 1, j = 1, g = 2)
-# 
-# # Q122
-# calcQ(N = N.matrix, i = 1, j = 2, g = 2)
-# 
-# # P212 = 1 - Q212
-# calcP(N = N.matrix, i = 2, j = 1, g = 2)
-# 
-# # Q222
-# calcQ(N = N.matrix, i = 2, j = 2, g = 2)
-
+# Probability of *not* observing allele i in population j given sample of g 
+# genes
+# Nj <- sum(N[ ,j])
+# Nij <- N[i, j]
 calcQ <- function(Nj, Nij, g) {
-  # Nj <- sum(N[ ,j])
-  # Nij <- N[i, j]
+  if (Nj <= (Nij + g - 1)) {
+    return(0)
+  }
   prod <- 1
   iter.g <- 0
   while (iter.g < g && prod > 0) {
@@ -61,8 +31,40 @@ calcQ <- function(Nj, Nij, g) {
   return(prod)
 }
 
+################################################################################
+# Probability of observing allele i in population j given sample of g genes
+# Nj <- sum(N[ ,j])
+# Nij <- N[i, j]
 calcP <- function(Nj, Nij, g) {
   prob <- 1 - calcQ(Nj = Nj, Nij = Nij, g = g)
+  return(prob)
+}
+
+################################################################################
+# Probability of *not* observing allele in population given sample of g 
+# genes
+# N.col is vector of allele counts for population j
+calcQ.v <- function(N.col, g) {
+  if(length(N.col) < 2 || sum(N.col) == 0) {
+    return(NA)
+  }
+  Nj <- sum(N.col)
+  product <- rep(x = 1, times = length(N.col))
+  iter.g <- 0
+  while (iter.g < g && sum(product) > 0) {
+    numerator <- Nj - N.col - iter.g
+    denominator <- Nj - iter.g
+    product <- product * (numerator / denominator)
+    iter.g <- iter.g + 1
+  }
+  return(product)
+}
+
+################################################################################
+# Probability of observing allele in population given sample of g genes
+# N.col is vector of allele counts for population j
+calcP.v <- function(N.col, g) {
+  prob <- 1 - calcQ.v(N.col = N.col, g = g)
   return(prob)
 }
 
@@ -74,28 +76,28 @@ printArgs <- function(Nj, Nij, g, standin.for.nij) {
   return(calcP(Nj = Nj, Nij = Nij, g = g))
 }
 
+################################################################################
+# Make N be a vector, not a matrix
+# calcRichness <- function(N, j, g) {
 calcRichness <- function(N, j, g) {
+  if(require(package = "plyr")) {
+    stop("ERROR: Richness calculations requires plyr package.")
+  }
+  richness <- 0
+  m <- nrow(N)
+  for (i in 1:m) {
+    richness <- richness + calcP(Nj = sum(N[ ,j]), Nij = N[i, j], g = g)
+  }
+  # return(richness)
+
+  aaply(.data = N, .margins = 2, .fun = calcP, Nj = 1, Nij = 2, g = 2)
+  
   ## TODO: here, but stumbling on apply / aaply
   ## Try first with for loop, then attempt a *ply approach
   # apply(X = N, MARGIN = 1, FUN = printArgs, Nj = 1, Nij = 2, g = 2)
-  richness <- aaply(.data = N, .margins = 2, .fun = printArgs, Nj = sum(N.matrix[, j]), g = 2, standin.for.nij = N.matrix[i])
-  return(sum(richness))
+  # richness <- aaply(.data = N, .margins = 2, .fun = printArgs, Nj = sum(N[, j]), g = 2, standin.for.nij = N[i, j])
+  # return(sum(richness))
   # p.vector <- apply(X = N, MARGIN = 1, FUN = calcP, Nj = sum(x), Nij = x[j], g = g)
   # return(sum(p.vector))
 }
-calcRichness(N = N.matrix, j = 1, g = 2)
 
-## TESTS
-N.matrix <- matrix(data = c(3, 6, 1, 0), nrow = 2, ncol = 2, byrow = TRUE)
-
-# P112 = 1 - Q112
-calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[1, 1], g = 2)
-
-# Q122
-calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[1, 2], g = 2)
-
-# P212 = 1 - Q212
-calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[2, 1], g = 2)
-
-# Q222
-calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[2, 2], g = 2)
---
+++ scripts/rarefaction-test.R
@@ -0,0 +1,42 @@
+# Rarefaction testing
+# Jeff Oliver
+# jcoliver@email.arizona.edu
+# 2017-05-17
+
+################################################################################
+rm(list = ls())
+
+source(file = "functions/rarefaction.R")
+
+## TESTS
+N.matrix <- matrix(data = c(3, 6, 1, 0), nrow = 2, ncol = 2, byrow = TRUE)
+
+calcRichness(N = N.matrix, j = 1, g = 2)
+calcRichness(N = N.matrix, j = 2, g = 2)
+calcRichness(N = N.matrix, j = 1, g = 4)
+calcRichness(N = N.matrix, j = 2, g = 4)
+
+# P112 = 1 - Q112
+calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[1, 1], g = 2)
+
+# Q122
+calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[1, 2], g = 2)
+
+# P212 = 1 - Q212
+calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[2, 1], g = 2)
+
+# Q222
+calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[2, 2], g = 2)
+
+# Prob allele *not* found
+calcQ.v(N.col = N.matrix[, 2], g = 2)
+calcQ.v(N.col = N.matrix[, 1], g = 2)
+
+# Prob allele found
+calcP.v(N.col = N.matrix[, 2], g = 2)
+calcP.v(N.col = N.matrix[, 1], g = 2)
+
+# Prob for single allele **doesn't work if N.col = 0 (for allele not found in population)
+# CANNOT work, because Nj (total # of genes surveyed from population is not included)
+calcP.v(N.col = N.matrix[1, 2], g = 2)
+calcP.v(N.col = N.matrix[2, 2], g = 2)
