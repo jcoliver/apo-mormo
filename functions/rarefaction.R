4a30c3a686445170d0258762a0aa7e96cd70ad52
jcoliver <jcoliver@email.arizona.edu>
2017-05-18 11:33:49 -0700

    Remove deprecated functions

--- functions/rarefaction.R
+++ functions/rarefaction.R
@@ -8,58 +8,15 @@
 # Kalinowski's algorithms for correcting for uneven sampling to estimate allele 
 # richness and private allele frequency
 
-# Probability of *not* observing allele i in population j given sample of g 
-# genes
-# Nj <- sum(N[ ,j])
-# Nij <- N[i, j]
-# DEPRECATED
-calcQ <- function(Nj, Nij, g) {
-  if (Nj <= (Nij + g - 1)) {
-    return(0)
-  }
-  prod <- 1
-  u <- 0
-  while (u < g && prod > 0) {
-    numerator <- Nj - Nij - u
-    if (numerator == 0) {
-      prod <- 0
-    } else {
-      denominator <- Nj - u
-      prod <- prod * (numerator / denominator)
-      u <- u + 1
-    }
-  }
-  return(prod)
-}
-
 ################################################################################
-# Probability of observing allele i in population j given sample of g genes
-# Nj <- sum(N[ ,j])
-# Nij <- N[i, j]
-# DEPRECATED
-calcP <- function(Nj, Nij, g) {
-  prob <- 1 - calcQ(Nj = Nj, Nij = Nij, g = g)
-  return(prob)
-}
-
-################################################################################
-# Calculate allele richness for population j given a sample of g genes
-# N is matrix of allele counts for all populations, with alleles as rows and 
-# populations as columns
-# DEPRECATED
-calcRichness <- function(N, j, g) {
-  richness <- 0
-  m <- nrow(N)
-  for (i in 1:m) {
-    richness <- richness + calcP(Nj = sum(N[ ,j]), Nij = N[i, j], g = g)
-  }
-  return(richness)
-}
-
-################################################################################
-# Probability of *not* observing allele in single population given sample of g 
-# genes
-# N.col is vector of allele counts for population j
+#' Probability of not observing alleles in single population given sample of g 
+#'   genes
+#' @param N.col vector of allele counts for a single population
+#' @param g gene sample size; generally the number of haploids or twice the 
+#'   number of diploid individuals in the smallest population sample size
+#' @description Calculates the probability of not observing each allele in a 
+#'   given population for a rarefied sample of size \code{g}
+#' @return vector of probabilities for single population
 calcQ.v <- function(N.col, g) {
   if(length(N.col) < 2 || sum(N.col) == 0) {
     return(NA)
@@ -87,35 +44,55 @@ calcQ.v <- function(N.col, g) {
 }
 
 ################################################################################
-# Probability of observing allele in single population given sample of g genes
-# N.col is vector of allele counts for population j
+#' Rarefied allele probability
+#' @param N.col vector of allele counts for a single population
+#' @param g gene sample size; generally the number of haploids or twice the 
+#'   number of diploid individuals in the smallest population sample size
+#' @description Calculates the probability of observing each allele in a given 
+#'   population for a rarefied sample of size \code{g}
+#' @return vector of probabilities for single population
 calcP.v <- function(N.col, g) {
   prob <- 1 - calcQ.v(N.col = N.col, g = g)
   return(prob)
 }
 
 ################################################################################
-# Calculate allele richness for a single population given sample of g genes
-# N.col is vector of allele counts for population j
+#' Rarefied allele richness for a single population
+#' @param N.col vector of allele counts for a single population
+#' @param g gene sample size; generally the number of haploids or twice the 
+#'   number of diploid individuals in the smallest population sample size
+#' @description Calculates allele richness for a rarefied sample of size 
+#'   \code{g}
+#' @return Allele richness in a single population rarefied to size \code{g}
 calcRichness.v <- function(N.col, g) {
   sum.richness <- sum(calcP.v(N.col = N.col, g = g))
   return(sum.richness)
 }
 
 ################################################################################
-# Calculate allele richness in each population
-# N is matrix of allele counts for all populations, with alleles as rows and 
-# populations as columns
-#' Calculate allele richness for each population
+#' Rarefied allele richness for each population
 #' @param N matrix of allele counts for all populations, with alleles as rows 
-#' and populations as columns
+#'   and populations as columns
 #' @param g gene sample size; generally the number of haploids or twice the 
-#' number of diploid individuals in the smallest population sample size
+#'   number of diploid individuals in the smallest population sample size
 #' @description Calculates allele richness for a rarefied sample of size 
-#' \code{g}
+#'   \code{g}
 #' @return A vector of allele richness, where each element is the richness for 
-#' a single population indexed by columns in \code{N}
+#'   a single population indexed by columns in \code{N}
 calcRichness.all <- function(N, g) {
   all.rich <- apply(X = N, MARGIN = 2, FUN = function(x) {calcRichness.v(N.col = x, g = g)})
   return(all.rich)
+}
+
+################################################################################
+#' Rarefied private allele count
+#' 
+calcPrivate <- function(N.col, g) {
+  # sum over all alleles
+  # Pijg * [(]prod from j'=1 to J, j' != j (Qij'g)]
+  m <- length(N.col)
+  private.sum <- 0
+  Pijg <- calcP.v(N.col = N.col, g = g) # a vector of allele probabilities
+  # Now we need a vector of the Q product
+
 }
--- scripts/rarefaction-test.R
+++ scripts/rarefaction-test.R
@@ -11,23 +11,6 @@ source(file = "functions/rarefaction.R")
 ## TESTS
 N.matrix <- matrix(data = c(3, 6, 1, 0), nrow = 2, ncol = 2, byrow = TRUE)
 
-calcRichness(N = N.matrix, j = 1, g = 2)
-calcRichness(N = N.matrix, j = 2, g = 2)
-calcRichness(N = N.matrix, j = 1, g = 4)
-calcRichness(N = N.matrix, j = 2, g = 4)
-
-# P112 = 1 - Q112
-calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[1, 1], g = 2)
-
-# Q122
-calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[1, 2], g = 2)
-
-# P212 = 1 - Q212
-calcP(Nj = sum(N.matrix[, 1]), Nij = N.matrix[2, 1], g = 2)
-
-# Q222
-calcQ(Nj = sum(N.matrix[, 2]), Nij = N.matrix[2, 2], g = 2)
-
 # Prob allele *not* found
 calcQ.v(N.col = N.matrix[, 2], g = 2)
 calcQ.v(N.col = N.matrix[, 1], g = 2)
