f9a2fc7f8571a9f4dc9a6a76edb9ddb32213c2f0
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-07-11 16:10:21 -0700

    Add tests on larger data sets ~1000 loci

---
+++ tests/rarefaction-test-11.R
@@ -0,0 +1,47 @@
+# Rarefaction test 11: Testing print frequency on 20 loci
+# Jeff Oliver
+# jcoliver@email.arizona.edu
+# 2017-07-11
+
+rm(list = ls())
+
+library("adegenet")
+
+source(file = "functions/rarefaction.R")
+genind.file = "output/genind-object.RData"
+load(file = genind.file) # apo.str.genind
+
+################################################################################
+# Identify those populations were are interested in
+pops <- c(1, 9, 11, 12, 13)
+pop.rows <- which(as.integer(as.character(apo.str.genind@pop)) %in% pops)
+
+# Subset data for testing (using a list not an S4 object)
+test.data = list()
+test.data$tab <- apo.str.genind@tab[pop.rows, 1:39]
+test.data$loc.fac <- factor(apo.str.genind@loc.fac[1:39])
+test.data$pop <- factor(apo.str.genind@pop[pop.rows])
+
+rarefied.values <- rarefiedMatrices(data = test.data, 
+                                    g = 10, 
+                                    display.progress = TRUE)
+richness.matrix <- rarefied.values$richness
+private.matrix <- rarefied.values$private
+
+################################################################################
+richness.test <- 90.36368
+private.test <- 7.367924
+################################################################################
+richness.matrix.sum <- round(x = sum(x = richness.matrix, na.rm = TRUE), 
+                             digits = 5)
+richness.test <- round(x = richness.test, digits = 5)
+private.matrix.sum <- round(x = sum(x = private.matrix, na.rm = TRUE), 
+                            digits = 5)
+private.test <- round(x = private.test, digits = 5)
+
+pass <- richness.matrix.sum == richness.test &&
+  private.matrix.sum == private.test
+
+cat("Test 11 pass = ", pass, "\n", sep = "")
+
+rm(list = ls())
