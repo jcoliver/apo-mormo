23703cdab854992b9f32a1182e76082047ab18e9
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-07-31 16:03:37 -0700

    Remove obsolete files

--- scripts/amova.R
+++
@@ -1,8 +0,0 @@
-# AMOVA on A. mormo data
-# Jeff Oliver
-# jcoliver@email.arizona.edu
-# 2016-11-23
-
-################################################################################
-
-# See: https://popgencode.wordpress.com/2015/04/08/hierarchical-and-per-locus-amova-using-r-adegenet-and-poppr/
